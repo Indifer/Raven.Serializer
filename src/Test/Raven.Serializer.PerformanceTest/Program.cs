f963ae455271bafe8cf40d28cf72274109eb38e9
LiangYi <liangyi@mallcoo.cn>
2015-12-17 19:17:04 +0800

    add test

---
+++ src/Test/Raven.Serializer.PerformanceTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.Serializer.PerformanceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+}
---
+++ src/Test/Raven.Serializer.Test/MsgPackTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Raven.Serializer.Test
+{
+    [TestClass]
+    public class MsgPackTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
