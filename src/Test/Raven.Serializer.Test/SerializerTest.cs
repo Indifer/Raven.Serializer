152143c191bbd051ffc5e134cce767aab2e8ca52
Indifer <indifer@126.com>
2016-01-18 18:08:08 +0800

    1

--- src/Test/Raven.Serializer.Test/SerializerTest.cs
+++ src/Test/Raven.Serializer.Test/SerializerTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MsgPack.Serialization;
+using Raven.Serializer.PerformanceTest;
 
 namespace Raven.Serializer.Test
 {
@@ -59,15 +60,42 @@ namespace Raven.Serializer.Test
         public void JilTest()
         {
             string val = "abc个";
-            byte[] buffer = new byte[7];
+            //byte[] buffer = new byte[7];
 
             IDataSerializer serializer = SerializerFactory.Create(SerializerType.Jil);
 
             var data = serializer.Serialize(val);
-            CollectionAssert.AreEqual(data, buffer);
+            //CollectionAssert.AreEqual(data, buffer);
 
-            var val_res = serializer.Deserialize<string>(buffer);
-            Assert.AreEqual(val, val_res);
+            var val_res = serializer.Deserialize<string>(data);
+            Assert.AreEqual(val, val_res);            
+
+            Mall mall = new Mall()
+            {
+                ID = 1,
+                Name = "大悦城",
+                GroupID = 135,
+                AAAAAAAAAA = "aaaa",
+                BBBBBBBBBB = "BBBB",
+                CCCCCCCCCC = "hygfjrt7kuylkhgliu;oi;yhdhtfjhsj",
+                D = "kuykj687jrstskhgfk",
+                EEEEEEEEEE = "jhlhlgjhkuykjuyt",
+                F = "djsgfjdjg",
+                G = "fdsgasdgs",
+                HHHHHHHHHH = "hgfdhergfdhs",
+                I = "fdjnhterjrgtas",
+                J = "fdhs5htrjgfdfdg",
+                User = new User()
+                {
+                    Date = DateTime.Now,
+                    ID = 132414,
+                    Name = "ggsgshahsahsdha"
+                }
+            };
+
+            var json = serializer.Serialize(mall);
+            var mall2 = serializer.Deserialize<Mall>(json);
+            Assert.AreEqual(mall.Name, mall2.Name);
         }
     }
 }
